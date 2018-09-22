6bc54d40aee498c0073e3cea71c551999d7b9965
louisbichard <contact@louisjohnbichard.co.uk>
2018-09-22 16:28:39 +0100

    udpated with making first request

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const port = 1993;
+const makeRequest = require('./makeRequest');
 
-app.get('/', async (req, res) => {
-
-  try {
-    const data = await axios.get('https://rs.aspsp.ob.forgerock.financial/open-banking/mtlsTest', {
-    'Postman-Token': '3d43b021-9308-4124-82ab-64cfc9119f3e',
-     'Cookie': 'cookiename=werewrw;cook',
-     'Pragma': 'no-cache',
-     'Cache-Control': 'no-cache'
-   });
-    res.status(200).send(data.data);
-  } catch(e) {
-    res.status(500).send('API failed');
-  }
-
-});
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+makeRequest({ path: '/open-banking/mtlsTest' })
+  .then( (data) => {
+    console.log(data);
+  });
